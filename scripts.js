2d803dbb596d44740af42ea4610204a450e0bc2d
Magda Balun <magi@op.pl>
2017-12-12 00:30:22 +0100

    Change index.html, style.css and scripts.js file

--- scripts.js
+++ scripts.js
@@ -2,34 +2,34 @@ $(function () {
 	var carouselList = $('#carousel .carousel-list'),
 		nextButton = $('#next'),
 		prevButton = $('#prev');
-			
+
 	var intervalId = null;
 	function setTimerInterval() {
-		intervalId = setInterval(changeSlides, 4000);
+		intervalId = setInterval(changeSlides, 2000);
 	}
 	setTimerInterval();
 
 	function changeSlides() {
-		carouselList.animate({'marginLeft': -800}, 500, moveFirstSlide());
-		activeSlides();
+		carouselList.animate({'marginLeft': '-800'},500, moveFirstSlide);
 	};
 
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
-		var item = carouselList.find("li");
-
+		
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft: 0});
+		carouselList.css({'marginLeft': '0'});
+		activeSlides();
 	};
 
 	function changeSlidesLeft() {
 		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-
-		lastItem.remove().css({'marginLeft': -800});
-		firstItem.before(lastItem);
-		lastItem.animate({'marginLeft': 0}, 500);
+        var lastItem = carouselList.find("li:last");
+        
+        firstItem.before(lastItem);
+        lastItem.animate({'marginLeft': 0}, 500);
+        carouselList.css({marginLeft:0});
+		activeSlides();
 	};
 
 	//controlers
@@ -42,29 +42,27 @@ $(function () {
 		changeSlidesLeft();
 	});
 	
-	
 	//indicators
+	clickedDots.click(function() {
+		clearInterval(intervalId);
+		dotShowSlides();
+	});
+
 
 	function activeSlides() {
-			var currentSlide = carouselList.find("li:first");
-			var selectedDots = $('.dots');
-			var currentSlideNumber = currentSlide.attr('data-slide');
+		var currentSlide = carouselList.find("li:first");
+		var selectedDots = $('.dots');
+		var currentSlideId = currentSlide.attr('data-slide');
 
-			selectedDots.each(function(index, element) {
-				if(index == currentSlideNumber) {
-					$(element).addClass('active');
-				} else {
-					$(element).removeClass('active');
-				}	
-		 	});
-	};		 	
+		selectedDots.each(function(index, element) {
+			if((index + 1) == currentSlideId) {
+				$(element).addClass('active');
+			} else {
+				$(element).removeClass('active');
+			}	
+		 });
+	}
 
 });
-
-
-
-	
-
-
 
 
