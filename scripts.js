71b07b9b7a097ba52aef7c9bd864c9105c7f8fd8
Magda Balun <magi@op.pl>
2017-12-12 10:00:27 +0100

    Change index.html, style.css and scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,68 +1,92 @@
-$(function () {
-	var carouselList = $('#carousel .carousel-list'),
-		nextButton = $('#next'),
-		prevButton = $('#prev');
+$(function() {
+    var carouselList = $('#carousel .carousel-list'),
+        nextButton = $('#next'),
+        prevButton = $('#prev');
 
-	var intervalId = null;
-	function setTimerInterval() {
-		intervalId = setInterval(changeSlides, 2000);
-	}
-	setTimerInterval();
+    var interval = null;
 
-	function changeSlides() {
-		carouselList.animate({'marginLeft': '-800'},500, moveFirstSlide);
-	};
+    function setTimerInterval() {
+        interval = setInterval(changeSlides, 3000);
+    }
+    setTimerInterval();
 
-	function moveFirstSlide() {
-		var firstItem = carouselList.find("li:first");
-		var lastItem = carouselList.find("li:last");
-		
-		lastItem.after(firstItem);
-		carouselList.css({'marginLeft': '0'});
-		activeSlides();
-	};
+    function changeSlides() {
+        carouselList.animate({
+            'marginLeft': -800
+        }, 500, moveFirstSlide);
+    };
 
-	function changeSlidesLeft() {
-		var firstItem = carouselList.find("li:first");
-        var lastItem = carouselList.find("li:last");
-        
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+
+        lastItem.after(firstItem);
+        carouselList.css({
+            'marginLeft': '0'
+        });
+        activeSlides();
+    };
+
+    function changeSlidesLeft() {
+        var firstItem = carouselList.find("li:first"),
+            lastItem = carouselList.find("li:last");
+
         firstItem.before(lastItem);
-        lastItem.animate({'marginLeft': 0}, 500);
-        carouselList.css({marginLeft:0});
-		activeSlides();
-	};
+        lastItem.animate({
+            'marginLeft': 0
+        }, 500);
+        carouselList.css({
+            marginLeft: 0
+        });
+        activeSlides();
+    };
 
-	//controlers
-	nextButton.click(function() {
-		clearInterval(intervalId);
-		changeSlides();
-	});
-	prevButton.click(function() {
-		clearInterval(intervalId);
-		changeSlidesLeft();
-	});
-	
-	//indicators
-	clickedDots.click(function() {
-		clearInterval(intervalId);
-		dotShowSlides();
-	});
+    //controlers
+    nextButton.click(function() {
+        clearInterval(interval);
+        changeSlides();
+        setTimerInterval();
+    });
+    prevButton.click(function() {
+        clearInterval(interval);
+        changeSlidesLeft();
+        setTimerInterval();
+    });
 
+    //indicators
+    function activeSlides() {
+        var currentSlide = carouselList.find("li:first"),
+            selectedDot = $('.dots'),
+            currentSlideId = currentSlide.attr('data-slide');
 
-	function activeSlides() {
-		var currentSlide = carouselList.find("li:first");
-		var selectedDots = $('.dots');
-		var currentSlideId = currentSlide.attr('data-slide');
+        selectedDot.each(function(index, element) {
+            if ((index + 1) == currentSlideId) {
+                $(element).addClass('active');
+            } else {
+                $(element).removeClass('active');
+            }
+        });
+    }
 
-		selectedDots.each(function(index, element) {
-			if((index + 1) == currentSlideId) {
-				$(element).addClass('active');
-			} else {
-				$(element).removeClass('active');
-			}	
-		 });
-	}
+    var selectedDot = $('.dots');
+    selectedDot.click(function() {
+        var firstItem = carouselList.find("li:first"),
+            currentSlide = firstItem.attr('data-slide'),
+            clickedDot = $(this).attr('data-slide');
+
+        clearInterval(interval);
+        if (currentSlide < clickedDot) {
+            var numberOfSlides = clickedDot - currentSlide;
+            for (var i = 0; i < numberOfSlides; i++) {
+                moveFirstSlide();
+            }
+        } else if (currentSlide > clickedDot) {
+            var numberOfSlides = currentSlide - clickedDot;
+            for (var i = 0; i < numberOfSlides; i++) {
+                changeSlidesLeft();
+            }
+        }
+        setTimerInterval();
+    });
 
 });
-
-
